912ea469c0b3e1616a92cb4186a3336bf2e494d2
Rashaad0554 <rashaad0554@gmail.com>
2024-10-27 09:20:41 -0400

    Added killing and dying

--- skull-search/scripts/player.gd
+++ skull-search/scripts/player.gd
@@ -9,48 +9,54 @@ var enemy_in_range = false
 var enemy_attack_cooldown = true
 var health = 100
 var player_alive = true
+var player_hit = false
 
 var attack_ip = false
+var hit_ip = false
+func _ready():
+	print("start")
 
 func _physics_process(delta: float) -> void:
 	enemy_attack()
-	player_attack()
-	if health <= 0:
-		#$AnimatedSprite2D.play("death")
-		health = 0
-		player_alive = false
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-#Get the input direction 1, 0 -1
-	var direction := Input.get_axis("move_left", "move_right")
-
-#flip the sprite
-	if direction>0:
-		animated_sprite.flip_h = false
-	elif direction <0:
-		animated_sprite.flip_h = true
-
-	if attack_ip:
-		if not animated_sprite.is_playing():
-			attack_ip = false
-	elif is_on_floor():
-		if direction == 0:
-			animated_sprite.play("idle")
-		elif direction:
-			animated_sprite.play("walking")
+	if !player_alive:
+		$AnimatedSprite2D.play("death")
 	else:
-		animated_sprite.play("jumping")
-	#play animation
+		player_attack()
+		if not is_on_floor():
+			velocity += get_gravity() * delta
+		# Handle jump.
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		#Get the input direction 1, 0 -1
+		var direction := Input.get_axis("move_left", "move_right")
 
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		#flip the sprite
+		if direction>0:
+			animated_sprite.flip_h = false
+		elif direction <0:
+			animated_sprite.flip_h = true
+		
+		if hit_ip:
+			if not animated_sprite.is_playing():
+				hit_ip = false
+		elif attack_ip:
+			if not animated_sprite.is_playing():
+				attack_ip = false
+		elif is_on_floor():
+			if direction == 0:
+				animated_sprite.play("idle")
+			elif direction:
+				animated_sprite.play("walking")
+		else:
+			animated_sprite.play("jumping")
+		#play animation
 
-	move_and_slide()
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+
+		move_and_slide()
 	
 	
 func player_move(delta):
@@ -85,10 +91,18 @@ func _on_hitbox_body_exited(body: Node2D) -> void:
 		
 func enemy_attack():
 	if enemy_in_range and enemy_attack_cooldown == true:
-		health = health - 20
+		if health-20 > 0:
+			$AnimatedSprite2D.play("hurt")
+			$RecieveAttackCD.start()
+			player_hit = true
+			hit_ip = true
+			health = health - 20
+		else:
+			health = 0
+			player_alive = false
 		enemy_attack_cooldown = false
 		$attackCD.start()
-		print(health)
+		print("player: ", health)
 
 func player():
 	pass
@@ -112,9 +126,17 @@ func player_attack():
 		if dir == 0:
 			animated_sprite.play("attack")
 			$DealAttackCD.start()
-			
 
 func _on_deal_attack_cd_timeout() -> void:
 	$DealAttackCD.stop()
 	global.player_current_attack = false
 	attack_ip = false
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if !player_alive:
+		get_tree().reload_current_scene()
+
+func _on_recieve_attack_cd_timeout() -> void:
+	$RecieveAttackCD.stop()
+	player_hit = false
+	hit_ip = false
